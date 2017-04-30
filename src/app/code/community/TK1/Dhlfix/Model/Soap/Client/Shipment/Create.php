ffea6fd3957e464fa02daeb9e6d791ce25524451
HenKun <hk@nebelkind.com>
2017-04-30 14:36:22 +0200

    v1.2.0 - Additional check for Packstation format when creating shipment, without need to edit manually

---
+++ src/app/code/community/TK1/Dhlfix/Model/Soap/Client/Shipment/Create.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TK1_Dhlfix_Model_Soap_Client_Shipment_Create
+ *
+ * @category  Model
+ * @package   TK1_Dhlfix
+ * @author    Henning Kuntzschmann
+ * @copyright Copyright (c) 2017 TK1 GmbH
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class TK1_Dhlfix_Model_Soap_Client_Shipment_Create extends Dhl_Intraship_Model_Soap_Client_Shipment_Create
+{
+    protected function _normalizeReceiverAddress(Mage_Sales_Model_Order_Address &$address)
+    {
+		/* @var $address Mage_Sales_Model_Order_Address */
+		// Introduces an additional check for a special "Packstation" format like "AccountId Packstation StationId". 
+		// Oddly enough this check is done when editing the address manually but not when the shipment is created without manual edit.
+		if (preg_match('/^(\d+)\s(packstation\s\d+)$/i', $address->getStreet(1), $match)) {
+					$address->setIdNumber($match[1])
+                    $address->setStationId($match[2]);					
+		}
+		else
+		{
+			parent::_normalizeReceiverAddress($address);
+		}
+    }
+}
