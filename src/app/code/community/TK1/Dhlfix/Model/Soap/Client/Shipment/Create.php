1213679e738484100cd3e6330425f58b16e66f2b
HenKun <hk@nebelkind.com>
2017-06-17 12:09:00 +0200

    [Fix] 1.3.1: Depending on Settings, M2ePro sets the first street address as company. If tis is the DHL account number it is not recognized. this solves this.

--- src/app/code/community/TK1/Dhlfix/Model/Soap/Client/Shipment/Create.php
+++ src/app/code/community/TK1/Dhlfix/Model/Soap/Client/Shipment/Create.php
@@ -13,11 +13,16 @@ class TK1_Dhlfix_Model_Soap_Client_Shipment_Create extends Dhl_Intraship_Model_S
     protected function _normalizeReceiverAddress(Mage_Sales_Model_Order_Address &$address)
     {
 		/* @var $address Mage_Sales_Model_Order_Address */
-		// Introduces an additional check for a special "Packstation" format like "AccountId Packstation StationId". 
+		// Introduces an additional check for a special "Packstation" format like "AccountId Packstation StationId" and "Packstation StationId" when Conpany contains AccountID. 
 		// Oddly enough this check is done when editing the address manually but not when the shipment is created without manual edit.
 		if (preg_match('/^(\d+)\s(packstation\s\d+)$/i', $address->getStreet(1), $match)) {
-					$address->setIdNumber($match[1]);
-                    $address->setStationId($match[2]);					
+			$address->setIdNumber($match[1]);
+            $address->setStationId($match[2]);					
+		}
+		elseif (preg_match('/^(\d+)$/i', $address->getCompany(), $match) && false !== stripos($address->getStreet(1), 'packstation'))
+		{
+			$address->setIdNumber($match[1]);
+            $address->setStationId($address->getStreet(1));		
 		}
 		else
 		{
